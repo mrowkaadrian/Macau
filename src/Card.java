7f5133c1982c36661218d03c9a23e0c51a4edd20
Adrian Mrówka <adrianmrowka00@gmail.com>
2017-12-12 17:46:38 +0100

    Added Card.java
    Added card enumeration

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+
+        public enum CardValue {
+            TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER
+        }
+
+        public enum CardColor {
+            HEART, DIAMOND, CLUB, SPADE
+        }
+
+        
+}
