26fae3410da4b0c01493210540b802033f6c206b
Adrian Mrówka <adrianmrowka00@gmail.com>
2017-12-31 23:07:22 +0100

    expanded enums (added "values" and their getters)

--- src/Card.java
+++ src/Card.java
@@ -3,11 +3,60 @@ public class Card {
 // ====== ENUMERATION ======
 
     public enum CardValue {
-        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), JOKER(14);
+
+        private final int value;
+        CardValue(int value) {
+            this.value = value;
+        }
+
+        static public CardValue returnValue(int value) {
+            switch (value) {
+                case 2: return TWO;
+                case 3: return THREE;
+                case 4: return FOUR;
+                case 5: return FIVE;
+                case 6: return SIX;
+                case 7: return SEVEN;
+                case 8: return EIGHT;
+                case 9: return NINE;
+                case 10: return TEN;
+                case 11: return JACK;
+                case 12: return QUEEN;
+                case 13: return KING;
+                case 14: return JOKER;
+
+                default: return null;
+            }
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     public enum CardColor {
-        HEART, DIAMOND, CLUB, SPADE
+        HEART(0), DIAMOND(1), CLUB(2), SPADE(3);
+
+        private final int value;
+        CardColor(int value) {
+            this.value = value;
+        }
+
+        static public CardColor returnColor(int value) {
+            switch (value) {
+                case 0: return HEART;
+                case 1: return DIAMOND;
+                case 2: return CLUB;
+                case 3: return SPADE;
+
+                default: return null;
+            }
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
 // ====== VARIABLES ======
